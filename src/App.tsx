3206b0ed9e914991cdcb19b3d2c03f39a76617c4
crawraps <crawraps@gmail.com>
2022-04-30 01:30:24 +0300

    Add initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Layout from './components/layouts/Layout'
+import About from './pages/About'
+import Home from './pages/Home'
+import Project from './pages/Project'
 
 function App(): JSX.Element {
-  return <></>
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='project/:id' element={<Project />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
