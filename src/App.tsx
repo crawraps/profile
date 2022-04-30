5b8d9cb2bb2c9be5fa1b5fe455df0c984e1722f9
crawraps <crawraps@gmail.com>
2022-04-30 16:33:56 +0300

    Moved providers from app to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
-import { store, useAppSelector } from './store'
+import { useAppSelector } from './store'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './colorscheme'
 
@@ -14,17 +13,15 @@ function App(): JSX.Element {
   const themeName = useAppSelector(state => state.theme)
 
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme[themeName]}>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='project/:id' element={<Project />} />
-          </Route>
-        </Routes>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme[themeName]}>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='project/:id' element={<Project />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './locales/config'
+import { BrowserRouter } from 'react-router-dom'
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
